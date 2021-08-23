df7dbccf578ccba5973473bd3f99b017010f3648
Illya Fedorovych <illya.fedorovych@gmail.com>
2021-08-23 17:34:33 +0300

    Fix/actions (#54)
    * Downgrade ubuntu image from latest to 20.04
    
    * Enclose docker buildx arguments into quotes
    
    * Check if archive entry is a directory, add console logs to stream end listeners
    
    * Move from SAX to xml-to-json-stream to simplify parsing process
    
    * Add test properties on undefined
    
    * Change fetch to request for zip download
    
    * Change request to axios for zip download
    
    * Change to unzip-stream and request for zip download and unpacking
    
    * Change request to axios for zip download
    
    * Make unzipper work with local file instead of download stream
    
    * Revert "Make unzipper work with local file instead of download stream"
    
    This reverts commit 810f9cd0427e69b6d50a4ca138da53d3dedc463a.
    
    * Increase request timeout for file download
    
    * Use request-retry-stream
    
    * Handle non-repeated fields flattening
    
    * Remove throttling, delay and timeout
    
    * Set timeout
    
    * Set log function for debug
    
    * Change to unxip-stream
    
    * Add throttle and delay
    
    * Remove throttle and delay, decrease timeout
    
    * Use nodejs-file-downloader to download zip instead of streaming it
    
    * Remove process.exit on bq stream finish
    
    * Cleanup code, remove obsolete dependencies
    
    * Change runner config back to cron
    
    * Remove request-retry-stream
    
    Co-authored-by: Suhich <miansu01@gmail.com>

--- index.js
+++ index.js
@@ -1,33 +1,59 @@
 require('dotenv').config();
 
 const fetch = require('node-fetch');
-const unzipper = require('unzipper');
+const unzipper = require('unzip-stream');
+const path = require('path');
 
 const { DATA_URL } = require('./config/download');
 const { getTable, insertData } = require('./bigquery');
 const bqConfig = require('./config/bigquery');
+const Downloader = require('nodejs-file-downloader');
 
 const downloadFile = async () => {
   const res = await fetch(DATA_URL);
   const json = await res.json();
   const url = json.result.resources.map(r => r.url).find(url => url.includes('full'));
 
-  const fileReq = await fetch(url);
-  fileReq.body.pipe(unzipper.Parse())
-    .on('entry', function (entry) {
-      const tables = ['uo', 'fop'];
-
-      console.log('got file', entry.path);
-      const table = tables.find(table => entry.path.toLowerCase().includes(table));
-
-      if (table) {
-        const tableConfig = bqConfig[table];
-        getTable(tableConfig).then(() => insertData(entry, tableConfig));
-      } else {
-        console.log('ERROR: Table for file', entry.path, 'is not found');
-        entry.autodrain();
+  let prevPercent = 0;
+  const downloader = new Downloader({
+    url,
+    directory: './downloads',
+    onProgress: (percentage) => {
+      if (Math.floor(percentage) !== prevPercent) {
+        prevPercent = Math.floor(percentage);
+        console.log(percentage, '%');
       }
-    });
+    },
+  });
+  try {
+    await downloader.download();
+    console.log('Download finished!');
+
+    const filename = url.split('/').pop();
+    require('fs').createReadStream('./downloads/' + filename).pipe(unzipper.Parse())
+      .on('entry', function (entry) {
+        const tables = ['uo', 'fop'];
+
+        console.log('got file', entry.path);
+
+        if (entry.type !== 'File') {
+          console.log('Entry type is ' + entry.type + ', skipping');
+          return;
+        }
+
+        const table = tables.find(table => path.basename(entry.path).toLowerCase().includes(table));
+
+        if (table) {
+          const tableConfig = bqConfig[table];
+          getTable(tableConfig).then(() => insertData(entry, tableConfig));
+        } else {
+          console.log('ERROR: Table for file', entry.path, 'is not found');
+          entry.autodrain();
+        }
+      });
+  } catch (error) {
+    console.log('Download failed', error);
+  }
 };
 
 downloadFile();
