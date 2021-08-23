8312a16f56475c5a9dc80bd3925bf8561acba6f4
kowalski0805 <illya.fedorovych@gmail.com>
2021-08-23 14:39:42 +0300

    Use nodejs-file-downloader to download zip instead of streaming it

--- index.js
+++ index.js
@@ -8,33 +8,54 @@ const rrs = require('request-retry-stream');
 const { DATA_URL } = require('./config/download');
 const { getTable, insertData } = require('./bigquery');
 const bqConfig = require('./config/bigquery');
+const Downloader = require('nodejs-file-downloader');
 
 const downloadFile = async () => {
   const res = await fetch(DATA_URL);
   const json = await res.json();
   const url = json.result.resources.map(r => r.url).find(url => url.includes('full'));
 
-  rrs.get(url, {timeout: 1000, logFunction: console.warn}).pipe(unzipper.Parse())
-    .on('entry', function (entry) {
-      const tables = ['uo', 'fop'];
-
-      console.log('got file', entry.path);
-
-      if (entry.type !== 'File') {
-        console.log('Entry type is ' + entry.type + ', skipping');
-        return;
+  let prevPercent = 0;
+  const downloader = new Downloader({
+    url,
+    directory: "./downloads",
+    onProgress: (percentage) => {
+      if (Math.floor(percentage) !== prevPercent) {
+        prevPercent = Math.floor(percentage);
+        console.log('% ', percentage);
       }
+    }
+  })
+  try {
+    await downloader.download();
+    console.log('Download finished!');
 
-      const table = tables.find(table => path.basename(entry.path).toLowerCase().includes(table));
+    const filename = url.split('/').pop();
+    // rrs.get(url, {timeout: 1000, logFunction: console.warn}).pipe(unzipper.Parse())
+    require('fs').createReadStream('./downloads/'+filename).pipe(unzipper.Parse())
+      .on('entry', function (entry) {
+        const tables = ['uo', 'fop'];
 
-      if (table) {
-        const tableConfig = bqConfig[table];
-        getTable(tableConfig).then(() => insertData(entry, tableConfig));
-      } else {
-        console.log('ERROR: Table for file', entry.path, 'is not found');
-        entry.autodrain();
-      }
-    });
+        console.log('got file', entry.path);
+
+        if (entry.type !== 'File') {
+          console.log('Entry type is ' + entry.type + ', skipping');
+          return;
+        }
+
+        const table = tables.find(table => path.basename(entry.path).toLowerCase().includes(table));
+
+        if (table) {
+          const tableConfig = bqConfig[table];
+          getTable(tableConfig).then(() => insertData(entry, tableConfig));
+        } else {
+          console.log('ERROR: Table for file', entry.path, 'is not found');
+          entry.autodrain();
+        }
+      });
+    } catch (error) {
+      console.log('Download failed',error)
+    }
 };
 
 downloadFile();
